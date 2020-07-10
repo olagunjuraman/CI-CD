3471456fd6cce6304b7d089ef8ae9a1089553899
princeraman <olagunjuraman@gmail.com>
2020-07-10 18:03:51 +0100

    CI/CD

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+    if(req.url === '/'){
+        res.write('Hello world');
+        res.end();
+    }
+});
+
+server.listen('3000');
+
+console.log('listening on port ')

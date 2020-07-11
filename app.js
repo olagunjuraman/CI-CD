8bae21f5d67d0a1d6edc201a55d0e640494e0373
princeraman <olagunjuraman@gmail.com>
2020-07-11 09:54:59 +0100

    no sabig

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer(function(req, res){
     if(req.url === '/'){
-        res.write('Hello world crazy motherfucker');
+        res.write('Hello world crazy motherfuckeffr');
         res.end();
     }
 });

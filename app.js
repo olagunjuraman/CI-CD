72783c5a9eb55657b30f6fa8df0efc7093a3ccdf
princeraman <olagunjuraman@gmail.com>
2020-07-11 09:49:49 +0100

    no sabi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer(function(req, res){
     if(req.url === '/'){
-        res.write('Hello world');
+        res.write('Hello world crazy motherfucker');
         res.end();
     }
 });
